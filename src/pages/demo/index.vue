5692cfa8d48a769689f761f587fbc63b759b39b7
fwj <231887453@qq.com>
2023-03-02 22:11:10 +0800

    初始化vite-vue项目并安装eslint、prettierrc、stylelint、husky

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>123</div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.test {
+  width: 110px;
+  height: 1239px;
+  color: aliceblue;
+
+  & .btn {
+    position: relative;
+    width: 20px;
+    height: 20px;
+  }
+}
+</style>
